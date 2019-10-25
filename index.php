3501c827fc6936777a6eb70a54da1450305ea656
liz-lit <art.pri-ngti@yandex.ru>
2019-10-25 11:26:58 +0700

    Add a password to DB connection

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 require_once __DIR__. '/vendor/autoload.php';
 //include "./classes/Students.php";
-$db_connect=pg_connect("host=localhost port=5432 dbname=postgres user=postgres");
+$db_connect=pg_connect("host=localhost port=5432 dbname=postgres user=postgres password =12345");
 
 $students = new Students($db_connect);
 $req_res = null;
